c865252d7daa20aaee5bcf95ec7cdd9794893732
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-04-22 22:39:20 -0400

    complete step one

---
+++ todo/src/Reducers.js/UseReducer.js
@@ -0,0 +1,12 @@
+import React, { useReducer } from "react";
+
+const toDoItems = {
+  item: "Learn about reducers",
+  completed: false,
+  id: 3892987589,
+};
+const reduce = (state, action) => {
+  return state;
+};
+
+export { toDoItems, reduce };
