cb66ae4d24fd9e20a7708490a4d52cdfa959f7c4
Jennifer Weiner <58009503+weinerjm14@users.noreply.github.com>
2020-04-24 16:02:15 -0400

    Updated wording on button

--- todo/src/components/ToDoList.js
+++ todo/src/components/ToDoList.js
@@ -2,22 +2,23 @@ import React, { useReducer } from "react";
 
 function ToDoList(props) {
   console.log("todolist props", props);
-  const clearDone = (e) => {
+  const clearDone = e => {
     e.preventDefault();
     props.dispatch({ type: "CLEAR_COMPLETE" });
   };
 
   return (
     <div>
-      {props.item.map((item) => (
+      {props.item.map(item => (
         <li
           onClick={() => props.toggle(item.id)}
           className={`todo${item.completed ? " done" : ""}`}
-          key={item.id}>
+          key={item.id}
+        >
           ToDo: {item.item}
         </li>
       ))}
-      <button onClick={clearDone}>GET RID OF SHIT</button>
+      <button onClick={clearDone}>Clear Done</button>
     </div>
   );
 }
